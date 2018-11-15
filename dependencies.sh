ea2ee24597773bfc0fd41fc32bdf98576680b76a
Kyle Muñoz <ttap2tv@gmail.com>
2018-11-15 22:22:27 +0100

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,9 +1,9 @@
 dnf -y install i3
 dnf -y install conky
 dnf -y install feh
-wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
-rpm -Uvh rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
-dnf -y install cmus
+#wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
+#rpm -Uvh rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
+#dnf -y install cmus
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps
 autoreconf --force --install
