ad87199d0def1eb70deb9b697f9b7301f876a8b3
Kyle Muñoz <ttap2tv@gmail.com>
2018-11-15 22:29:54 +0100

    Update and rename dependencies.sh to thisfirst.sh

--- thisfirst.sh
+++ thisfirst.sh
@@ -1,9 +1,6 @@
 dnf -y install i3
 dnf -y install conky
 dnf -y install feh
-#wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
-#rpm -Uvh rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
-#dnf -y install cmus
 dnf -y install libxcb-devel xcb-util-keysyms-devel xcb-util-devel xcb-util-wm-devel xcb-util-xrm-devel yajl-devel libXrandr-devel startup-notification-devel libev-devel xcb-util-cursor-devel libXinerama-devel libxkbcommon-devel libxkbcommon-x11-devel pcre-devel pango-devel git gcc automake
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps
