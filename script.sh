af9d6d80f12e4d5109073a6d2906937ed320032d
Kyle Munoz <lace22@localhost.localdomain>
2019-09-24 18:08:54 +0200

    vdsafdsa

--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+sudo su
+dnf -y install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf -y install cmus
 dnf -y install i3
 dnf -y install conky
 dnf -y install feh
@@ -11,3 +15,11 @@ mkdir -p build && cd build/
 make
 sudo make install
 echo "Done"
+exit
+cp wall.jpg $HOME
+cp -r bin $HOME
+cp -r .config $HOME
+cp .conkyrc $HOME
+cp .Xdefaults $HOME
+cp compton.conf $HOME
+echo $HOME
